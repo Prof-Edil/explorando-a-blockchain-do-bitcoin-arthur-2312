3e90370c85d70ba747ac04efa8b8c97823e61939
arthur-2312 <159627086+arthur-2312@users.noreply.github.com>
2025-02-02 22:59:55 -0300

    003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,4 +1,4 @@
 # How many new outputs were created by block 123,456?
 hsh=$(bitcoin-cli getblockhash 123456)
-outputs=$(bitcoin-cli getblock $hsh 1 | jq "[.tx[].vout | length] | add")
+outputs=$(bitcoin-cli getblock $hsh 2 | jq "[.tx[].vout | length] | add")
 echo $outputs
