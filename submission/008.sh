734c55dbf892765873a834eb756145b7d3c6de01
arthur-2312 <159627086+arthur-2312@users.noreply.github.com>
2025-02-02 22:57:43 -0300

    008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,12 @@
 # Which public key signed input 0 in this tx:
 #   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+TX_ID="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+
+DETALHES_TRANSACAO=$(bitcoin-cli getrawtransaction $TX_ID 1)
+
+#   - txinwitness[0] → Assinatura
+#   - txinwitness[1] → Chave pública
+#   - txinwitness[2] → Chave pública usada na assinatura
+CHAVE_PUBLICA_ASSINANTE=$(echo $DETALHES_TRANSACAO | jq -r '.vin[0].txinwitness[2][4:70]')
+
+echo "Chave pública usada na assinatura: $CHAVE_PUBLICA_ASSINANTE"
